ded9a3a784fc51e338c066777ac8afd28b734d15
Kai Kuller <kavakai@gmail.com>
2022-11-16 13:43:56 -0700

    Add method to change value displayed on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calculator from './Components/Calculator';
 import './css.css'
 
 function App() {
+  const [displayValue, setDisplayValue] = useState(0);
+  const [currentValue, setCurrentValue] = useState(0);
 
   const handleClick = (e:any) => {
-    console.log(e.target.value)
+    setDisplayValue(e.target.value)
   }
 
   return (
     <div className="App">
       <h1>The Second Best Calculator In The World!</h1>
-      <Calculator handleClick={handleClick}/>
+      <Calculator 
+        handleClick={handleClick} 
+        displayValue={displayValue} 
+        setDisplayValue={setDisplayValue}
+        currentValue={currentValue}
+        setCurrentValue={setCurrentValue}
+      />
     </div>
   );
 }
--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -3,6 +3,10 @@ import Numbers from './Numbers';
 
 export interface IAppProps {
  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+ displayValue: any;
+ setDisplayValue: (value: any) => void;
+ currentValue: number;
+ setCurrentValue: (value: number) => void; 
 }
 
 export default function Calculator (props: IAppProps) {
@@ -10,7 +14,7 @@ export default function Calculator (props: IAppProps) {
 
   return (
     <div className='whole-calculator'>
-      <span className='screen-display' data-role={"display"}>0</span>
+      <div id="screen" className='screen-display' data-role={"display"}>{props.displayValue || props.currentValue}</div>
       <div className='all-btns'>
         <Numbers handleClick={props.handleClick}/>
         <button onClick={(e) => props.handleClick(e)} value={0}>0</button>
